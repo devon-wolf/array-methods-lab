3ebb04c73a4453a784eb45133ed5f0f10be7e50d
Devon Wolfkiel <devonwolf@tutanota.com>
2021-03-23 15:43:34 -0700

    Add index param to callback in every

--- every.js
+++ every.js
@@ -1,6 +1,6 @@
 const every = (array, callback) => {
-	for(let i of array) {
-		if(!callback(i)) return false;
+	for(let i = 0; i < array.length; i++) {
+		if(!callback(array[i], i)) return false;
 	}
 	return true;
 }
