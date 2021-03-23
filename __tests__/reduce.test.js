932ee085805cd9859e09475c44e40a94bf303c72
Devon Wolfkiel <devonwolf@tutanota.com>
2021-03-23 14:40:31 -0700

    Finish reduce function and tests

--- __tests__/reduce.test.js
+++ __tests__/reduce.test.js
@@ -1,12 +1,18 @@
-// const reduce = require('../reduce.js');
+const reduce = require('../reduce.js');
 
-describe.skip('reduce function', () => {
+describe('reduce function', () => {
 	it('returns the final value of an accumulator based on the contents of an array and a callback function', () => {
 		const original = [1, 2, 3, 4, 5, 6];
-		
+		const callback = (a, b) => a + b;
+		const initialValue = 40;
+
+		expect(reduce(original, callback, initialValue)).toEqual(61);
 	});
 
 	it('uses the first item of the array as the initial value of the accumulator if no initial value is supplied', () => {
+		const original = [1, 2, 3, 4, 5, 6];
+		const callback = (a, b) => a + b;
 
+		expect(reduce(original, callback)).toEqual(21);
 	});
 });
--- reduce.js
+++ reduce.js
@@ -1,5 +1,18 @@
-const reduce = (array, callback, initialValue) {
+const reduce = (array, callback, initialValue) => {
+	let accumulator = initialValue || array[0];
 	
+	if(!initialValue) {
+		for (let i = 1; i < array.length; i++) {
+			accumulator = callback(accumulator, array[i]);
+		}
+	}
+	else {
+		for (let i of array) {
+			accumulator = callback(accumulator, i);
+		}
+	}
+	
+	return accumulator;
 };
 
 module.exports = reduce;
