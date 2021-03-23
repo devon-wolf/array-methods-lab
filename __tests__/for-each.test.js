036db934c361d2493fbba3a675b028723ba150c5
Devon Wolfkiel <devonwolf@tutanota.com>
2021-03-23 15:14:08 -0700

    Add forEach function and tests

---
+++ __tests__/for-each.test.js
@@ -0,0 +1,18 @@
+const forEach = require('../for-each.js');
+
+describe('for each function', () => {
+	it('calls the callback function for each item in the array', () => {
+		original = [1, 2, 3, 4, 5];
+		callback = jest.fn();
+
+		forEach(original, callback);
+
+		expect(callback).toHaveBeenCalledTimes(5);
+		
+		expect(callback).toHaveBeenNthCalledWith(1, 1, 0);
+		expect(callback).toHaveBeenNthCalledWith(2, 2, 1);
+		expect(callback).toHaveBeenNthCalledWith(3, 3, 2);
+		expect(callback).toHaveBeenNthCalledWith(4, 4, 3);
+		expect(callback).toHaveBeenNthCalledWith(5, 5, 4);
+	});
+});
